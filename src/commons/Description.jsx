7d8f5f1b77bec32134c5e14669f857217b66d9c3
lucas h <lucas.ph@live.com>
2023-07-02 01:15:09 -0300

    se editan y crean componentes

---
+++ src/commons/Description.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { useLocation, useParams } from "react-router-dom";
+
+export const Description = () => {
+  const id = useParams();
+  const result = useLocation().state;
+  console.log(result);
+
+  return (
+    <div className="container-description">
+      <div className="card-description">
+        <img
+          src={`https://image.tmdb.org/t/p/original${result.poster_path}`}
+          alt="poster"
+          className="poster"
+        />
+        <img
+          src={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
+          alt="backdrop"
+          className="backdrop"
+        />
+        <div className="data-description">
+          <h2>{result.name ? result.name : result.title}</h2>
+          <p>{result.overview}</p>
+          <p>
+            <span>Valoracion: </span>
+            {result.vote_average}
+          </p>
+          <div className="data-datos">
+            <p>
+              <span>Idioma:</span> {result.original_language.toUpperCase()}
+            </p>
+            <p>
+              <span>Fecha: </span>
+              {result.release_date}
+            </p>
+          </div>
+        </div>
+        <div className="data-play">
+          <button>
+            <svg
+              width="89px"
+              height="89px"
+              viewBox="-1.92 -1.92 27.84 27.84"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              stroke="#000000"
+            >
+              <g
+                id="SVGRepo_bgCarrier"
+                stroke-width="0"
+                transform="translate(4.08,4.08), scale(0.66)"
+              >
+                <rect
+                  x="-1.92"
+                  y="-1.92"
+                  width="27.84"
+                  height="27.84"
+                  rx="13.92"
+                  fill="#3800d3"
+                  strokewidth="0"
+                ></rect>
+              </g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke="#CCCCCC"
+                stroke-width="0.048"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <g id="Media / Play_Circle">
+                  {" "}
+                  <g id="Vector">
+                    {" "}
+                    <path
+                      d="M3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12Z"
+                      stroke="#f0f0f0"
+                      stroke-width="0.8160000000000001"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                    <path
+                      d="M10 15V9L15 12L10 15Z"
+                      stroke="#f0f0f0"
+                      stroke-width="0.8160000000000001"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                  </g>{" "}
+                </g>{" "}
+              </g>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/commons/MoviesAndTvs.jsx
+++ src/commons/MoviesAndTvs.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Unico from "./Home";
+import { Link, Navigate } from "react-router-dom";
 
 const MoviesAndTvs = () => {
   const [prop, setProp] = useState("");
@@ -36,7 +37,6 @@ const MoviesAndTvs = () => {
     e.preventDefault();
     fetchResult(searchKey);
   };
-
   useEffect(() => {
     fetchResult();
   }, [prop]);
@@ -63,7 +63,31 @@ const MoviesAndTvs = () => {
             placeholder="Buscar"
             onChange={(e) => setSearchKey(e.target.value)}
           />
-          <button>&#128270;</button>
+          <button>
+            <svg
+              width="64px"
+              height="64px"
+              viewBox="0 0 1024 1024"
+              class="icon"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#000000"
+              transform="rotate(0)matrix(-1, 0, 0, 1, 0, 0)"
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <path
+                  d="M448 768A320 320 0 1 0 448 128a320 320 0 0 0 0 640z m297.344-76.992l214.592 214.592-54.336 54.336-214.592-214.592a384 384 0 1 1 54.336-54.336z"
+                  fill="#3800d3"
+                ></path>
+              </g>
+            </svg>
+          </button>
         </form>
       </div>
       <div className="container">
@@ -74,10 +98,17 @@ const MoviesAndTvs = () => {
             </div>
 
             <div key={movieAndTv.id} className="img">
-              <img
-                src={`${urlImage + movieAndTv.poster_path}`}
-                className="img-poster"
-              />
+              <Link
+                to={{
+                  pathname: `/description/${movieAndTv.id}`,
+                }}
+                state={movieAndTv}
+              >
+                <img
+                  src={`${urlImage + movieAndTv.poster_path}`}
+                  className="img-poster"
+                />
+              </Link>
               <div className="vote">
                 <p>{movieAndTv.vote_average.toFixed(1)}</p>
               </div>
